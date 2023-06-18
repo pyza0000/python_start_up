5532f389c6feb2cd6920a5bd7ececccf092e6aa3
FilipP <pyza171@gmail.com>
2023-06-18 16:24:25 +0200

    0.3

--- python- start.py
+++ python- start.py
@@ -21,7 +21,6 @@ print(full_name.title())
 
 
 
-
 #białe znaki to spacje,tabulatory  etp
 
 favourite_language = 'python '
@@ -38,3 +37,26 @@ x, y, z = 1, 2, 3
 print(x, y, z)
 
 MAX_CONNECTIONS = 5000 # nazwy zapisane wielkimi literami są traktowane jako stałe! 
+
+
+
+
+#listy
+bicycles = ['trekingowy', 'górski', 'miejski', 'szosowy']
+print(bicycles[-2].title())
+
+print(f"Moim zdaniem najlepszym rowerem jest rower typu {bicycles[-1].title()}")
+
+bicycles[-2] = 'hybrydowy' # zmiana 
+print(bicycles[-2])
+
+bicycles.append('elektryczny') #method to add another element
+print(bicycles)
+
+cars = [] #create a list 
+cars.insert(0, 'ducati') #pozwala na stworzenie elementu w każdym możliwym miejscu listy, poprzez przesuwanie elementów
+print(cars)
+
+del bicycles[0] #można usunąć każdy element o ile znamy jego indeks, niema powrotu do tego elementu 
+print(bicycles)
+
